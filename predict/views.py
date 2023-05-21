221cd8a6ba7a907d360147c620e7bf5814e651c9
coder-dilip <designsdilip@gmail.com>
2023-05-21 12:45:30 +0545

    tic tac model added

--- predict/views.py
+++ predict/views.py
@@ -4,21 +4,17 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-
-
 def index(request):
     return render(request,'predict.html')
-
 
 import ast
 def predict(request):
     import tensorflow as tf
     import numpy as np
-
-    input_array = request.GET['input_array']
+    input_array = request.GET['input_array']   #"[1,2,3,4,5]"
     input_array=ast.literal_eval(input_array)
+    game = tf.keras.models.load_model('tictac_model')
     print(input_array)
-    game = tf.keras.models.load_model('myModel2')
     predict = game.predict(np.array([input_array]))
     prediction = int(np.argmax(predict))
 
