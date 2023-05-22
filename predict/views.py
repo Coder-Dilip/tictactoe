41e433f681c14f63ad09a5413f39c2b856b35c3c
coder-dilip <designsdilip@gmail.com>
2023-05-22 15:12:26 +0545

    added model source code

--- predict/views.py
+++ predict/views.py
@@ -3,18 +3,17 @@
 import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-import tensorflow as tf
-import numpy as np
 
 def index(request):
     return render(request,'predict.html')
 
 import ast
 def predict(request):
-    
+    import tensorflow as tf
+    import numpy as np
     input_array = request.GET['input_array']   #"[1,2,3,4,5]"
     input_array=ast.literal_eval(input_array)
-    game = tf.keras.models.load_model('myModel3')
+    game = tf.keras.models.load_model('tictac_model')
     print(input_array)
     predict = game.predict(np.array([input_array]))
     prediction = int(np.argmax(predict))
